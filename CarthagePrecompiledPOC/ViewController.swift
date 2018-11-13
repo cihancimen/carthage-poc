160278f0af61ca81a0e91d26c762c03b00353296
Cihan Cimen <CCimen@walmartlabs.com>
2018-11-13 10:11:23 -0800

    Initial Commit

---
+++ CarthagePrecompiledPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthagePrecompiledPOC
+//
+//  Created by Cihan Cimen on 11/13/18.
+//  Copyright © 2018 Walmartlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
